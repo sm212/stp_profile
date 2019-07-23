67eb0153f2df69411a51c39a441d37490d2de679
Sean <sean.maguire212@gmail.com>
2019-07-23 14:53:43 +0100

    added get_data function

--- helper_funcs.r
+++ helper_funcs.r
@@ -1,2 +1,24 @@
 library(tidyverse)
 library(fingertipsR)
+
+
+get_data = function(indicator_id, geog_type, out_path = './data/'){
+  # Get indicator data at a specified geographic level.
+  #
+  # Checks if file already exists in out_path folder. Function only downloads
+  # data if file not found.
+  #
+  # Saves files with the name <indicator_id>_<geog_type>.csv
+  
+  file_name = paste0(out_path, indicator_id, '_', geog_type, '.csv')
+  
+  if (file.exists(file_name)){
+    cat('File already exisits in', out_path, '\n')
+  }
+  else{
+    df = fingertips_data(IndicatorID = indicator_id,
+                         AreaTypeID = geog_type)
+    
+    write_csv(df, file_name)
+  }
+}
