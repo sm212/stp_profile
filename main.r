4beffb3d184aa1f00ea850f8ee506e2a993088cd
Sean <sean.maguire212@gmail.com>
2019-07-28 20:05:23 +0100

    loop over all combinations of sex & age when producing output

--- main.r
+++ main.r
@@ -17,18 +17,32 @@ for (i in 1:nrow(indicator_list)){
   area_codes = geog_lookup[as.character(geog_id)][[1]]
   
   
-  # Make plots
-  bar = plot_bars(df, area_codes)
-  ranks = rank_areas(df, area_codes)
-  best = head(ranks$AreaName, 1)
-  worst = tail(ranks$AreaName, 1)
-  trend = plot_trend(df, area_codes, c(best, worst, 'England'))
+  # Make outputs for each sex & age combination
+  sex_values = unique(df$Sex)
+  age_values = unique(df$Age)
   
-  # Write out
-  if (!dir.exists('./out/')) {
-    dir.create('./out/')
+  for (sex in sex_values){
+    for (age in age_values){
+      df_filtered = df %>%
+        filter(Sex == sex & Age == age)
+      
+      bar = plot_bars(df_filtered, area_codes)
+      ranks = rank_areas(df_filtered, area_codes)
+      best = head(ranks$AreaName, 1)
+      worst = tail(ranks$AreaName, 1)
+      trend = plot_trend(df_filtered, c(best, worst, 'England'))
+      
+      # Write out
+      if (!dir.exists('./out/')) {
+        dir.create('./out/')
+      }
+      
+      file_suffix = paste0('_', sex, '_', age)
+      file_suffix = str_replace(file_suffix, '[><]', '')
+      
+      ggsave(paste0('./out/bar', id, '_', geog_id, file_suffix, '.png'), bar)
+      ggsave(paste0('./out/trend', id, '_', geog_id, file_suffix, '.png'), trend)
+      write_csv(ranks, paste0('./out/rank', id, '_', geog_id, file_suffix, '.csv'))
+    }
   }
-  ggsave(paste0('./out/bar', id, '_', geog_id, '.png'), bar)
-  ggsave(paste0('./out/trend', id, '_', geog_id, '.png'), trend)
-  write_csv(ranks, paste0('./out/', id, '_', geog_id, '_ranks.csv'))
 }
